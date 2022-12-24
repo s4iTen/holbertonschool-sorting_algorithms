f6eb22f6bb3861c528f8e68f4b5288814c5e583a
s4iTen <ychouk102@gmail.com>
2022-12-24 13:16:43 +0100

    this function is the bubble sort of an array

---
+++ 0-bubble_sort.c
@@ -0,0 +1,20 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t new, new2;
+	int temp;
+
+	for (new = 0; new < size - 1; new++)
+	{
+		for (new2 = 0; new2 < size - new - 1; new2++)
+		{
+			if (array[new2] > array[new2 + 1])
+			{
+				temp = array[new2];
+				array[new2] = array[new2 + 1];
+				array[new2 + 1] = temp;
+			}
+		}
+	}
+}
