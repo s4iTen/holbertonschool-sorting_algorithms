4d25c8e242df2d302df6d17c2664cdfc44d55f37
s4iTen <ychouk102@gmail.com>
2022-12-25 00:09:13 +0100

    this function bubble sort the array and print it with function print_array

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,9 +2,12 @@
 
 void bubble_sort(int *array, size_t size)
 {
+	if (!array)
+	{
+		return;
+	}
 	size_t new, new2;
 	int temp;
-
 	for (new = 0; new < size - 1; new++)
 	{
 		for (new2 = 0; new2 < size - new - 1; new2++)
@@ -14,6 +17,7 @@ void bubble_sort(int *array, size_t size)
 				temp = array[new2];
 				array[new2] = array[new2 + 1];
 				array[new2 + 1] = temp;
+				print_array(array, size);
 			}
 		}
 	}
